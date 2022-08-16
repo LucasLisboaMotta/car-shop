6586d376ed89ddbe0f3ee1300b250eb62208ea62
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-16 16:43:16 -0300

    style: Alterando modo de exportação devido o esLint

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,9 +1,7 @@
-interface IModel<T> {
+export interface IModel<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[]>,
   readOne(id: string): Promise<T | null>,
   update(id: string, obj: T): Promise<T | null>,
   delete(id: string): Promise<T | null>
 }
-
-export { IModel };
