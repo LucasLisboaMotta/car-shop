d291705b984db3ea66a3a13572f98dac68014004
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-17 21:17:31 -0300

    Feat: Criando classe generica GenericService

---
+++ src/services/generic.service.ts
@@ -0,0 +1,50 @@
+import GenericModel from '../models/generic.model';
+import { IService } from './interfaces/generic.service';
+import CustomError, { 
+  invalid, 
+  minCharacters,
+  notFound,
+} from '../helpers/CustomError';
+import { TZod } from '../Types/TZod';
+
+export default abstract class GenericService<T> implements IService<T> {
+  private _model: GenericModel<T>;
+  private _zod: TZod<T>;
+
+  constructor(model: GenericModel<T>, zod: TZod<T>) {
+    this._model = model;
+    this._zod = zod;
+  }
+    
+  create(obj: T): Promise<T> {
+    const verifyObj = this._zod.safeParse(obj);
+    if (!verifyObj.success) throw new CustomError(invalid);
+    return this._model.create(obj);
+  }
+
+  read(): Promise<T[]> {
+    return this._model.read();
+  }
+
+  async readOne(id: string): Promise<T> {
+    if (id.length < 24) throw new CustomError(minCharacters);
+    const obj = await this._model.readOne(id);
+    if (!obj) throw new CustomError(notFound);
+    return obj;
+  }
+
+  async update(id: string, obj: T): Promise<T> {
+    if (id.length < 24) throw new CustomError(minCharacters);
+    const verifyObj = this._zod.safeParse(obj);
+    if (!verifyObj.success) throw new CustomError(invalid);
+    const objUpdated = await this._model.update(id, obj);
+    if (!objUpdated) throw new CustomError(notFound);
+    return objUpdated;
+  }
+
+  async delete(id: string): Promise<void> {
+    if (id.length < 24) throw new CustomError(minCharacters);
+    const obj = await this._model.delete(id);
+    if (!obj) throw new CustomError(notFound);
+  }
+}
