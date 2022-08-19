79c7f15fdbe72778423b52739c4484416e2f2e4d
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-19 16:14:20 -0300

    Feat: Adicionando parametro para remover chave de versionamento

--- src/models/schema/car.schema.ts
+++ src/models/schema/car.schema.ts
@@ -1,15 +1,18 @@
 import { Schema, model } from 'mongoose';
 import { ICar } from '../../interfaces/ICar';
 
-const carSchema = new Schema<ICar>({
-  model: { type: String, required: true },
-  year: { type: Number, required: true },
-  color: { type: String, required: true },
-  buyValue: { type: Number, required: true },
-  doorsQty: { type: Number, required: true },
-  seatsQty: { type: Number, required: true },
-  status: { type: Boolean, required: false },
-});
+const carSchema = new Schema<ICar>(
+  {
+    model: { type: String, required: true },
+    year: { type: Number, required: true },
+    color: { type: String, required: true },
+    buyValue: { type: Number, required: true },
+    doorsQty: { type: Number, required: true },
+    seatsQty: { type: Number, required: true },
+    status: { type: Boolean, required: false },
+  },
+  { versionKey: false },
+);
 
 const carMongooseModel = model<ICar>('car', carSchema);
 
