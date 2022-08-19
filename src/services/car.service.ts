d4eb281d04fd5ca3fa95742c0357ea04eef4f909
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-19 16:16:23 -0300

    Style: Alterando identação

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -2,6 +2,6 @@ import GenericService from './generic.service';
 import ICarService from './interfaces/car.service';
 import { ICar } from '../interfaces/ICar';
 
-class CarService extends GenericService<ICar> implements ICarService {}
-
-export default CarService;
+export default class CarService 
+  extends GenericService<ICar>
+  implements ICarService {}
