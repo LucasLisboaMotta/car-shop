bbf21c36eb62be7dff5cd34cb9ff02ea052ab40b
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-19 16:13:41 -0300

    Feat: Criando interface IMotorcycleModel

---
+++ src/models/interfaces/motorcycle.model.ts
@@ -0,0 +1,6 @@
+import { IMotorcycle } from '../../interfaces/IMotorcycle';
+import { IModel } from '../../interfaces/IModel';
+
+type IMotorcycleModel = IModel<IMotorcycle>;
+
+export default IMotorcycleModel;
