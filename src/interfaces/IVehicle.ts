601dc434ca169564750e44433a5fde0dd0e8b8b3
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-08-19 16:12:21 -0300

    Refactor: Criando interface e schema generica IVehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,13 @@
-export interface IVehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number
-}
+import { z } from 'zod';
+
+const VehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().min(1900).max(2022),
+  color: z.string().min(3),
+  buyValue: z.number().int(),
+  status: z.boolean().optional(),
+});
+
+type IVehicle = z.infer<typeof VehicleZodSchema>;
+
+export { VehicleZodSchema, IVehicle };
